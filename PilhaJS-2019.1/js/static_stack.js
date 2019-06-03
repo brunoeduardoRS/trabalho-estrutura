5d20ace8ceaaed9c665e25e474cfb53dde8c4edc
Halissongn2 <50643189+Halissongn2@users.noreply.github.com>
2019-06-02 23:42:42 -0300

    Add files via upload

--- PilhaJS-2019.1/js/static_stack.js
+++ PilhaJS-2019.1/js/static_stack.js
@@ -1,34 +1,34 @@
 class StaticStack {
 
     constructor(){
-        this.data = [];
+        this.data = [];//incializa o array
     }
     
     push (element) {
-        this.data.push(element);
+        this.data.push(element); //insere um elemento novo no alto da pilha.
     }
 
     pop() {
-        return this.data.pop();
+        return this.data.pop(); // retira o elemento superior da pilha 
     }
 
     peek() {
-        return this.data.length -1 ;
+        return this.data.length -1 ; //retorna o elemento do topo da lista -1
     }
 
-    isEmpty() {
-        return (this.size()===0);
+    isEmpty() { //verifica se existe algo na pilha
+        return (this.size()===0); //retorna se a pilha está vazia
     }
 
     size() {
-        return this.data.length;
+        return this.data.length;//retorna o tamanho da pilha
     }
 
-    clear() {
-        this.data = [];
+    clear() { //limpa a pilha
+        this.data = [];//limpa o array
     }
     
-    print(separator=' - ') {
+    print(separator=' - ') {//mostra toda a pilha com o separador
         /*  Another implementation
         if(this.isEmpty()) return "";
         let text = this.data[this.peek()];
@@ -37,10 +37,10 @@ class StaticStack {
         }
         return text; 
         */
-        let text = "";
-        for (let index = (this.data.length - 1); index >= 0; index--) {
-            text += this.data[index] + separator;
+        let text = ""; // cria uma variavel local
+        for (let index = (this.data.length - 1); index >= 0; index--) { // cria uma variavel local recebendo o tamanho do array menos 1, enquanto o valor de index for maior que zero será decrementado -1
+            text += this.data[index] + separator;// variavel text recebe ela mesma e a variavel index com o separador 
         }
-        return text.substr(0, text.length - separator.length);
+        return text.substr(0, text.length - separator.length); // retorna a variavel text com o separador menos depois do ultimo elemento da variavel text
     }
 }
